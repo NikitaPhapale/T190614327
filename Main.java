705ec003ccb508046665699ac2109f33df9d5f0e
Nikita Phapale <113188061+NikitaPhapale@users.noreply.github.com>
2024-05-11 11:33:34 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main{
 public static void main(String args[]){
    Scanner sc=new Scanner(System.in);
-   System.out.println("Enter value of a:);
+   System.out.println("Enter value of a:");
    int a=sc.nextInt();
    System.out.println("The value of a is:"+a);
 }
